af1432315ddc81cab9783168bc7420fae7b29f01
Benedict <benedictryan80@gmail.com>
2023-04-23 00:11:22 +0700

    optimize code 2

--- components/FormValidation.js
+++ components/FormValidation.js
@@ -2,11 +2,6 @@ import { useForm } from 'react-hook-form';
 import Error from './Error';
 import { schema } from '@/utils/form-validation';
 import { yupResolver } from '@hookform/resolvers/yup';
-import {
-	CreditCardIcon,
-	CalendarDaysIcon,
-	LockClosedIcon
-} from '@heroicons/react/24/outline';
 import Field from './elements/Field';
 import CardField from './elements/CardField';
 
@@ -20,32 +15,45 @@ const FormValidation = () => {
 		resolver: yupResolver(schema)
 	});
 
-	const handleSubmitForm = (formValues) => {
-		console.log(formValues);
-	};
+	const handleSubmitForm = (formValues) => console.log(formValues);
 
 	const resetForm = () => reset();
+
+	const {
+		username,
+		email,
+		card_number,
+		expire_date,
+		cvv,
+		dob,
+		occupation,
+		dev,
+		file_upload,
+		password,
+		confirm_password,
+		terms_and_conditions
+	} = errors;
 
 	return (
 		<form
 			onSubmit={handleSubmit(handleSubmitForm)}
 			className="flex flex-col gap-2 rounded-lg bg-neutral-50 p-8 text-neutral-900 shadow"
 		>
-			<Field label="Username" error={errors.username}>
+			<Field label="Username" error={username}>
 				<input
 					className="rounded border border-neutral-300 bg-neutral-50 p-1"
 					type="text"
 					{...register('username')}
 				/>
 			</Field>
-			<Field label="Email" error={errors.email}>
+			<Field label="Email" error={email}>
 				<input
 					className="rounded border border-neutral-300 bg-neutral-50 p-1"
 					{...register('email')}
 				/>
 			</Field>
 
-			<CardField label="Card number" error={errors.card_number}>
+			<CardField label="Card number" error={card_number}>
 				<input
 					className="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"
 					type="text"
@@ -55,8 +63,7 @@ const FormValidation = () => {
 				/>
 			</CardField>
 
-			<label className="relative flex-1 flex flex-col">
-				<span className="mb-1">Expire date</span>
+			<CardField label="Expire date" error={expire_date}>
 				<input
 					className="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"
 					type="text"
@@ -64,13 +71,9 @@ const FormValidation = () => {
 					placeholder="MM/YY"
 					{...register('expire_date')}
 				/>
-				<CalendarDaysIcon className="absolute bottom-0 left-0 -mb-0.5 transform translate-x-1/2 -translate-y-1/2 text-black peer-placeholder-shown:text-gray-300 h-6 w-6" />
-			</label>
-			{errors.expire_date && <Error message={errors.expire_date.message} />}
+			</CardField>
 
-			<label className="relative flex-1 flex flex-col">
-				<span className="mb-1">CVV</span>
-
+			<CardField label="CVV" error={cvv}>
 				<input
 					className="rounded-md peer pl-12 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"
 					type="text"
@@ -78,67 +81,64 @@ const FormValidation = () => {
 					placeholder="&bull;&bull;&bull;"
 					{...register('cvv')}
 				/>
-				<LockClosedIcon className="absolute bottom-0 left-0 -mb-0.5 transform translate-x-1/2 -translate-y-1/2 text-black peer-placeholder-shown:text-gray-300 h-6 w-6" />
-			</label>
-			{errors.cvv && <Error message={errors.cvv.message} />}
+			</CardField>
 
-			<label>Date of Birth</label>
-			<input
-				className="rounded border border-neutral-300 bg-neutral-50 p-1"
-				type="date"
-				{...register('dob')}
-			/>
-			{errors.dob && <Error message={errors.dob.message} />}
+			<Field label="Date of Birth" error={dob}>
+				<input
+					className="rounded border border-neutral-300 bg-neutral-50 p-1"
+					type="date"
+					{...register('dob')}
+				/>
+			</Field>
 
-			<label>Occupation</label>
-			<select
-				className="rounded border border-neutral-300 p-1"
-				name="occupation"
-				{...register('occupation')}
-			>
-				<option value="">Select</option>
-				<option value="Frontend Developer">Frontend Developer</option>
-				<option value="Freelancer">Freelancer</option>
-				<option value="Accountant">Accountant</option>
-				<option value="UI Engineer">UI Engineer</option>
-			</select>
-			{errors.occupation && <Error message={errors.occupation.message} />}
+			<Field label="Occupation" error={occupation}>
+				<select
+					className="rounded border border-neutral-300 p-1"
+					name="occupation"
+					{...register('occupation')}
+				>
+					<option value="">Select</option>
+					<option value="Frontend Developer">Frontend Developer</option>
+					<option value="Freelancer">Freelancer</option>
+					<option value="Accountant">Accountant</option>
+					<option value="UI Engineer">UI Engineer</option>
+				</select>
+			</Field>
 
-			<label className="mt-3">Developer</label>
-			<div className="flex">
-				<div className="flex mr-5">
-					<input
-						type="radio"
-						{...register('dev')}
-						name="dev"
-						value="Yes"
-						className="mr-1"
-					/>
-					<p>Yes</p>
+			<Field label="Developer" error={dev}>
+				<div>
+					<div className="flex">
+						<input
+							type="radio"
+							{...register('dev')}
+							name="dev"
+							value="Yes"
+							className="mr-1"
+						/>
+						<p>Yes</p>
+					</div>
+					<div className="flex">
+						<input
+							type="radio"
+							{...register('dev')}
+							name="dev"
+							value="No"
+							className="mr-1"
+						/>
+						<p>No</p>
+					</div>
 				</div>
-				<div className="flex">
-					<input
-						type="radio"
-						{...register('dev')}
-						name="dev"
-						value="No"
-						className="mr-1"
-					/>
-					<p>No</p>
-				</div>
-			</div>
-			{errors.dev && <Error message={errors.dev.message} />}
+			</Field>
 
-			<label className="block  mt-2 text-sm font-medium text-gray-900 ">
-				Upload file
-			</label>
-			<input
-				className="block w-full text-sm  border border-gray-300 rounded-lg cursor-pointer bg-gray-50"
-				type="file"
-				{...register('file_upload')}
-			/>
-			{errors.file_upload && <Error message={errors.file_upload.message} />}
-			<Field label="Password" error={errors.password}>
+			<Field label="Upload file" error={file_upload}>
+				<input
+					className="block w-full text-sm  border border-gray-300 rounded-lg cursor-pointer bg-gray-50"
+					type="file"
+					{...register('file_upload')}
+				/>
+			</Field>
+
+			<Field label="Password" error={password}>
 				<input
 					{...register('password')}
 					className="rounded border border-neutral-300 bg-neutral-50 p-1"
@@ -146,7 +146,7 @@ const FormValidation = () => {
 				/>
 			</Field>
 
-			<Field label="Confirm Password" error={errors.confirm_password}>
+			<Field label="Confirm Password" error={confirm_password}>
 				<input
 					{...register('confirm_password')}
 					className="rounded border border-neutral-300 bg-neutral-50 p-1"
@@ -162,9 +162,7 @@ const FormValidation = () => {
 				/>
 				<label>Accept Terms & Conditions</label>
 			</div>
-			{errors.terms_and_conditions && (
-				<Error message={errors.terms_and_conditions.message} />
-			)}
+			{terms_and_conditions && <Error message={terms_and_conditions.message} />}
 
 			<button
 				type="submit"
--- components/elements/Field.js
+++ components/elements/Field.js
@@ -1,13 +1,11 @@
 import Error from '../Error';
 
-const Field = ({ children, label, error }) => {
-	return (
-		<>
-			<label>{label}</label>
-			{children}
-			{error && <Error message={error.message} />}
-		</>
-	);
-};
+const Field = ({ children, label, error }) => (
+	<>
+		<label>{label}</label>
+		{children}
+		{error && <Error message={error.message} />}
+	</>
+);
 
 export default Field;
