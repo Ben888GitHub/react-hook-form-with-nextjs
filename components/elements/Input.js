e3bdbc1dcf3724a5d2561045e024ffa28bdfa611
Benedict <benedictryan80@gmail.com>
2023-04-23 23:54:40 +0700

    remove dead comments

--- components/elements/Input.js
+++ components/elements/Input.js
@@ -3,9 +3,4 @@ import { forwardRef } from 'react';
 const Input = (props, ref) => {
 	return <input ref={ref} {...props} />;
 };
-
-// export const Input = forwardRef((props, ref) => {
-//     return <input {...props} ref={ref} />
-// })
-
 export default forwardRef(Input);
